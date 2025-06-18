5a1741eeb9dab765dc1407f748a7ce0500431dab
ervz <ervin.mitra@playonsports.com>
2025-06-17 23:30:29 -0400

    req handler func()

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"encoding/json"
+	"log"
+	"nena-manipu-latina/models"
+	"net/http"
+	"time"
+)
+
+func handleAdRequest(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var adReq models.AdRequest
+	if err := json.NewDecoder(r.Body).Decode(&adReq); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Received request: %v\n", adReq)
+
+	// TODO: Auction, creative, logging
+	adResp := models.AdResponse{
+		AdMarkup: "<div>Your AD Here</div>",
+		CPM:      "1.25",
+		Bidder:   "mock-bidder",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(adResp)
+
+	log.Printf("Responded in %v\n", time.Since(start))
+}
+
+func main() {
+	http.HandleFunc("/serve", handleAdRequest)
+	port := ":8080"
+}
