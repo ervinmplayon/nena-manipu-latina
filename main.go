3385e79158f37fa03526e748a24a3dbca18ca5eb
ervz <ervin.mitra@playonsports.com>
2025-06-18 23:40:46 -0400

    use 8ball logger on request handler

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"nena-manipu-latina/models"
 	"net/http"
 	"time"
+
+	"github.com/ervinmplayon/intercour-face-loggizle/logger"
 )
+
+// ! Because 8-balls are neither good nor bad, its just mid asf
+var eight_ball_logger logger.Logger = &logger.LogrusLogger{}
 
 func handleAdRequest(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 
 	if r.Method != http.MethodPost {
+		/*
+		 * We are literally learning on the fly here
+		 * TODO: (move me somewhere like in a bible and give me dignity, wicked sorcerer)
+		 * IT is a standard paractive to log errors like malfromed requests.
+		 * `http.Error` writes the response to the client - but it doesn't tell now us (the server)
+		 * now does it? This is where the logger comes in.
+		 */
+		eight_ball_logger.Error(fmt.Sprintf("Invalid method: %s", r.Method))
 		http.Error(w, "Only POST is allowed", http.StatusMethodNotAllowed)
 		return
 	}
