68509d2b42f114074d9d9a823dda7b9d390bd7c7
ervz <ervin.mitra@playonsports.com>
2025-06-18 23:57:30 -0400

    implement eightball logger

--- main.go
+++ main.go
@@ -32,11 +32,21 @@ func handleAdRequest(w http.ResponseWriter, r *http.Request) {
 
 	var adReq models.AdRequest
 	if err := json.NewDecoder(r.Body).Decode(&adReq); err != nil {
+		eight_ball_logger.Error(fmt.Sprintf("Failed to decode JSON: %s", err.Error()))
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
 
-	log.Printf("Received request: %v\n", adReq)
+	/*
+	 * TODO: Same. Add to my book of spells. Goddamn Im a sorcerer.
+	 * %s String-> This verb is specifically used for printing string values. When I use %s
+	 * with avariable that is not a string, Go will attempt to convert it to a string
+	 * representation if possible.
+	 * %v Value -> This is a general-purpose format verb that prints the value in its default format.
+	 * It can be used for any data type and Go will determine the appropriate representation based on
+	 * its type. AKA structs are printed with their field values.
+	 */
+	eight_ball_logger.Info(fmt.Sprintf("Received Request: %v\n", adReq))
 
 	// TODO: Auction, creative, logging
 	adResp := models.AdResponse{
@@ -48,12 +58,13 @@ func handleAdRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(adResp)
 
-	log.Printf("Responded in %v\n", time.Since(start))
+	eight_ball_logger.Info(fmt.Sprintf("Responded in %v\n", time.Since(start)))
 }
 
 func main() {
 	http.HandleFunc("/serve", handleAdRequest)
 	port := ":8080"
-	log.Printf("Starting ad server on %s...\n", port)
+	eight_ball_logger.Info(fmt.Sprintf("Starting ad server on %s...\n", port))
+	// TODO: implement Fatal on logger side.
 	log.Fatal(http.ListenAndServe(port, nil))
 }
