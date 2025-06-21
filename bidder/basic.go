5a0ea2b2716160e75a7011274c02de66180df553
ervz <ervin.mitra@playonsports.com>
2025-06-21 13:34:32 -0400

    basic bidder

---
+++ bidder/basic.go
@@ -0,0 +1,19 @@
+package bidder
+
+import (
+	"fmt"
+	"nena-manipu-latina/models"
+)
+
+type BasicBidder struct {
+	Name string
+}
+
+func (b *BasicBidder) Bid(req models.AdRequest) models.AdResponse {
+	// TODO: Evolve from hardcoded bids
+	return models.AdResponse{
+		AdMarkup: fmt.Sprintf("<div>Buy from %s!</div>", b.Name),
+		CPM:      "1.23",
+		Bidder:   b.Name,
+	}
+}
