5a737e3db7fc531d6753e4d0e5b49653f2b6bb0e
ervz <ervin.mitra@playonsports.com>
2025-06-24 20:12:40 -0400

    defensive handling

--- auction/firstresponder.go
+++ auction/firstresponder.go
@@ -18,7 +18,7 @@ func (fr *FirstResponder) Auction(bids []bidder.Bidder, req models.AdRequest) mo
 		}(bb)
 	}
 	// ? In production, I want to protect against dead bidders. If a bidder does not respond,
-	// ? this can block forever, adding a timeout.
+	// ? this can block forever, adding a timeout aks Defensive Handling
 	select {
 	case res := <-resCh:
 		return res // * first to respond wins
