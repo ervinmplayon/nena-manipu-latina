87eb764c475bf955bffaca90e107e9e00ecb76c9
ervz <ervin.mitra@playonsports.com>
2025-06-26 14:59:53 -0400

    auction factory

--- auction/factory.go
+++ auction/factory.go
@@ -13,5 +13,12 @@ func Factory(name string) (AuctionStrategy, error) {
 	case RoundRobinStrategy:
 		eight_ball_logger.Info(fmt.Sprintf("Strategy Factory: Selected %s", RoundRobinStrategy))
 		return &RoundRobin{}, nil
+	case FirstResponderStrategy:
+		eight_ball_logger.Info(fmt.Sprintf("Strategy Factory: Selected %s", FirstResponderStrategy))
+		return &FirstResponder{}, nil
+	default:
+		err := fmt.Errorf("unknown strategy: %s", name)
+		eight_ball_logger.Error(fmt.Sprintf("Strategy Factory [ERROR]: %s", err))
+		return nil, err
 	}
 }
