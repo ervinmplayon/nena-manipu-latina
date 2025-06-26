ed3344d2bc5c6a0f1373b8b182117617ea8c4cb3
ervz <ervin.mitra@playonsports.com>
2025-06-26 15:11:31 -0400

    package level scoped logger

--- auction/factory.go
+++ auction/factory.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ervinmplayon/intercour-face-loggizle/logger"
 )
 
+// ? This is declared at the package level, it has package scope. It can be used anywhere in this package
 var eight_ball_logger logger.Logger = &logger.StandardLogger{}
 
 func Factory(name string) (AuctionStrategy, error) {
