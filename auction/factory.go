70c23dead92bcc9699bd8e2fde645427b4d8b287
ervz <ervin.mitra@playonsports.com>
2025-06-26 14:54:42 -0400

    roundrobin auction strat factoiry

---
+++ auction/factory.go
@@ -0,0 +1,17 @@
+package auction
+
+import (
+	"fmt"
+
+	"github.com/ervinmplayon/intercour-face-loggizle/logger"
+)
+
+var eight_ball_logger logger.Logger = &logger.StandardLogger{}
+
+func Factory(name string) (AuctionStrategy, error) {
+	switch name {
+	case RoundRobinStrategy:
+		eight_ball_logger.Info(fmt.Sprintf("Strategy Factory: Selected %s", RoundRobinStrategy))
+		return &RoundRobin{}, nil
+	}
+}
