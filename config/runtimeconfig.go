200d0b103b366400299cdd75f19391f36a6cc186
ervz <ervin.mitra@playonsports.com>
2025-06-28 23:30:13 -0400

    define publisher configurations

--- config/runtimeconfig.go
+++ config/runtimeconfig.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"nena-manipu-latina/auction"
+	"nena-manipu-latina/bidder"
+	"time"
+)
+
+// ? PublisherConfig is loaded per incoming ad request
+type PublisherConfig struct {
+	Strategy string
+	Bidders  []bidder.Bidder
+}
+
+// ? Fake static config mapping (can later be replaced by DB or Redis)
+var publisherConfigs = map[string]PublisherConfig{
+	"pub-123": {
+		Strategy: auction.FirstResponderStrategy,
+		Bidders: []bidder.Bidder{
+			&bidder.MockBidder{Name: "FastDSP", Delay: 20 * time.Millisecond, CPM: 1.05},
+			&bidder.MockBidder{Name: "ReliableDSP", Delay: 40 * time.Millisecond, CPM: 1.10},
+		},
+	},
+	"pub-abc": {
+		Strategy: auction.RoundRobinStrategy,
+		Bidders: []bidder.Bidder{
+			&bidder.MockBidder{Name: "SlowBidder", Delay: 100 * time.Millisecond, CPM: 0.99},
+			&bidder.MockBidder{Name: "AggressiveDSP", Delay: 50 * time.Millisecond, CPM: 1.30},
+		},
+	},
+}
