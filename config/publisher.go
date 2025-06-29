7db3138a902e4f2e4ab874adeed86a41ac5ef851
ervz <ervin.mitra@playonsports.com>
2025-06-29 00:40:30 -0400

    separate bidders[] logic away from publisher configs

--- config/publisher.go
+++ config/publisher.go
@@ -3,7 +3,6 @@ package config
 import (
 	"nena-manipu-latina/auction"
 	"nena-manipu-latina/bidder"
-	"time"
 )
 
 // ? PublisherConfig is loaded per incoming ad request
@@ -16,17 +15,11 @@ type PublisherConfig struct {
 var publisherConfigs = map[string]PublisherConfig{
 	"publisher_firstresponder": {
 		Strategy: auction.FirstResponderStrategy,
-		Bidders: []bidder.Bidder{
-			&bidder.MockBidder{Name: "FastDSP", Delay: 20 * time.Millisecond, CPM: 1.05},
-			&bidder.MockBidder{Name: "ReliableDSP", Delay: 40 * time.Millisecond, CPM: 1.10},
-		},
+		Bidders:  bidder.Factory("publisher_firstresponder"),
 	},
 	"publisher_roundrobin": {
 		Strategy: auction.RoundRobinStrategy,
-		Bidders: []bidder.Bidder{
-			&bidder.MockBidder{Name: "SlowBidder", Delay: 100 * time.Millisecond, CPM: 0.99},
-			&bidder.MockBidder{Name: "AggressiveDSP", Delay: 50 * time.Millisecond, CPM: 1.30},
-		},
+		Bidders:  bidder.Factory("publisher_roundrobin"),
 	},
 }
 
