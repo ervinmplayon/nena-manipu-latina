fd5b6e855d17e15944c00b22eeecd905a7bb4b5d
ervz <ervin.mitra@playonsports.com>
2025-06-29 00:34:34 -0400

    more descriptive key names

--- config/publisher.go
+++ config/publisher.go
@@ -14,14 +14,14 @@ type PublisherConfig struct {
 
 // ? Fake static config mapping (can later be replaced by DB or Redis)
 var publisherConfigs = map[string]PublisherConfig{
-	"pub-123": {
+	"publisher_firstresponder": {
 		Strategy: auction.FirstResponderStrategy,
 		Bidders: []bidder.Bidder{
 			&bidder.MockBidder{Name: "FastDSP", Delay: 20 * time.Millisecond, CPM: 1.05},
 			&bidder.MockBidder{Name: "ReliableDSP", Delay: 40 * time.Millisecond, CPM: 1.10},
 		},
 	},
-	"pub-abc": {
+	"publisher_roundrobin": {
 		Strategy: auction.RoundRobinStrategy,
 		Bidders: []bidder.Bidder{
 			&bidder.MockBidder{Name: "SlowBidder", Delay: 100 * time.Millisecond, CPM: 0.99},
