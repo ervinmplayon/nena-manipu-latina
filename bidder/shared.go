973d9d7b0a724160d2cca39fc6931fae1a67e5f6
ervz <ervin.mitra@playonsports.com>
2025-07-03 23:29:42 -0400

    bidder shared 8-ball

---
+++ bidder/shared.go
@@ -0,0 +1,6 @@
+package bidder
+
+import "github.com/ervinmplayon/intercour-face-loggizle/logger"
+
+// ? This is declared at the package level, it has package scope. It can be used anywhere in this package
+var eight_ball_logger logger.Logger = &logger.StandardLogger{}
