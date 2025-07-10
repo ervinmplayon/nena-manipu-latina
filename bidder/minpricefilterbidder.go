215e2b8fcf069fec5a08df90451db33eb399a9ac
ervz <ervin.mitra@playonsports.com>
2025-07-09 23:18:12 -0400

    call out for being cheap

--- bidder/minpricefilterbidder.go
+++ bidder/minpricefilterbidder.go
@@ -11,16 +11,17 @@ type MinPriceBidder struct {
 	MinPrice float64
 }
 
-func (m *MinPriceBidder) Bid(req models.AdRequest) models.AdResponse {
+func (m *MinPriceBidder) Bid(req models.AdRequest) (models.AdResponse, error) {
 	res, err := m.Inner.Bid(req)
 	if err != nil {
 		eight_ball_logger.Error(fmt.Sprintf("Min Price Bidder: Error on Bid() %s", err))
-		return models.AdResponse{}
+		return models.AdResponse{}, err
 	}
 
 	if res.CPM < m.MinPrice {
 		// Return empty response if invalid or too low
-		return models.AdResponse{}
+		eight_ball_logger.Info(fmt.Sprintf("Min Price Bidder: Too cheap damn it. Min Price is %v, CPM is %v", m.MinPrice, res.CPM))
+		return models.AdResponse{}, nil
 	}
-	return res
+	return res, nil
 }
--- bidder/mockbidder.go
+++ bidder/mockbidder.go
@@ -12,12 +12,12 @@ type MockBidder struct {
 	CPM   float64
 }
 
-func (mb *MockBidder) Bid(req models.AdRequest) models.AdResponse {
+func (mb *MockBidder) Bid(req models.AdRequest) (models.AdResponse, error) {
 	time.Sleep(mb.Delay)
 	eight_ball_logger.Info(fmt.Sprintf("[MOCK BIDDER] %s responding with CPM=%.2f after %s", mb.Name, mb.CPM, mb.Delay))
 	return models.AdResponse{
 		Bidder:   mb.Name,
 		CPM:      mb.CPM,
 		AdMarkup: "<div>Mock AD from " + mb.Name + "</div>",
-	}
+	}, nil
 }
