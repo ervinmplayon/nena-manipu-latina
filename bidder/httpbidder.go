6ceb0383040c12b2b7a115ad90e04825e1b3321c
ervz <ervin.mitra@playonsports.com>
2025-07-11 23:05:41 -0400

    HTTPBidder struct

---
+++ bidder/httpbidder.go
@@ -0,0 +1,22 @@
+package bidder
+
+import (
+	"net/http"
+	"time"
+)
+
+type HTTPBidder struct {
+	name     string
+	endpoint string
+	timeout  time.Duration
+	client   *http.Client
+}
+
+func NewHTTPBidder(name, endpoint string, timeout time.Duration) *HTTPBidder {
+	return &HTTPBidder{
+		name:     name,
+		endpoint: endpoint,
+		timeout:  timeout,
+		client:   &http.Client{},
+	}
+}
