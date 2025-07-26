0a6401428e9ff746ec75cdca42c913c161d1f20f
ervz <ervin.mitra@playonsports.com>
2025-07-25 22:19:05 -0400

    prep for context passing

--- auction/firstresponder.go
+++ auction/firstresponder.go
@@ -4,23 +4,22 @@ package auction
 
 import (
 	"errors"
-	"fmt"
 	"nena-manipu-latina/bidder"
 	"nena-manipu-latina/models"
-	"time"
 )
 
 type FirstResponder struct{}
 
+// TODO: rewrite to include context passing. Apply the same to the Auction() interface
 func (fr *FirstResponder) Auction(bidders []bidder.Bidder, req models.BidRequest) (models.BidResponse, error) {
-	results := collectBidsConcurrently(bidders, req, 50*time.Millisecond)
+	// results := collectBidsConcurrently(bidders, req, 50*time.Millisecond)
 
-	for _, res := range results {
-		if res.Err == nil && res.Response != nil {
-			eight_ball_logger.Info(fmt.Sprintf("First Responder: %s won the bid", res.Response.Bidder))
-			return *res.Response, nil // * First valid response
-		}
-	}
-	eight_ball_logger.Error("first responder: No valid bidder response")
+	// for _, res := range results {
+	// 	if res.Err == nil && res.Response != nil {
+	// 		eight_ball_logger.Info(fmt.Sprintf("First Responder: %s won the bid", res.Response.Bidder))
+	// 		return *res.Response, nil // * First valid response
+	// 	}
+	// }
+	// eight_ball_logger.Error("first responder: No valid bidder response")
 	return models.BidResponse{}, errors.New("first responder: No valid bidder response")
 }
