5521f904798ed60c52318beb860ceb3bd641d616
ervz <ervin.mitra@playonsports.com>
2025-07-28 22:16:04 -0400

    auction with context

--- auction/strategy.go
+++ auction/strategy.go
@@ -1,6 +1,7 @@
 package auction
 
 import (
+	"context"
 	"nena-manipu-latina/bidder"
 	"nena-manipu-latina/models"
 )
@@ -8,4 +9,9 @@ import (
 type AuctionStrategy interface {
 	// ? this will make sense as an auction takes ad responses and have 1 ad win
 	Auction(bids []bidder.Bidder, req models.BidRequest) (models.BidResponse, error)
+}
+
+// ? Pretty neat as this also returns partial errors
+type AuctionStrategyWithContext interface {
+	AuctionWithContext(ctx context.Context, bids []bidder.Bidder, req models.BidRequest) ([]*models.BidResponse, []error)
 }
