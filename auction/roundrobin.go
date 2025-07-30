0825d69a66f4124b77ed697c272425403eb95b49
ervz <ervin.mitra@playonsports.com>
2025-07-29 23:34:34 -0400

    round robin is now context-aware

--- auction/roundrobin.go
+++ auction/roundrobin.go
@@ -2,6 +2,7 @@
 package auction
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"nena-manipu-latina/bidder"
@@ -13,16 +14,16 @@ type RoundRobin struct {
 	index atomic.Uint64
 }
 
-func (rr *RoundRobin) Auction(bids []bidder.Bidder, req models.BidRequest) (models.BidResponse, error) {
+func (rr *RoundRobin) AuctionWithContext(ctx context.Context, bids []bidder.Bidder, req models.BidRequest) (*models.BidResponse, error) {
 	if len(bids) == 0 {
-		return models.BidResponse{}, errors.New("round Robin: no bidders available")
+		return &models.BidResponse{}, errors.New("round Robin: no bidders available")
 	}
 	i := rr.index.Add(1)
 	selected := bids[i%uint64(len(bids))]
-	resp, err := selected.Bid(req)
+	resp, err := selected.Bid(ctx, req)
 	if err != nil {
 		eight_ball_logger.Error(fmt.Sprintf("bidder %s failed: %s", selected.Name(), err.Error()))
-		return models.BidResponse{}, errors.New("round Robin bidder error")
+		return &models.BidResponse{}, errors.New("round Robin bidder error")
 	}
 	eight_ball_logger.Info(fmt.Sprintf("Round Robin: %s won the bid", resp.Bidder))
 	return resp, nil
